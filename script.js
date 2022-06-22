63f3e2374a686546585a67592220e2670512fe1d
Ahkal <bp@sojourn.llc>
2022-06-22 12:21:53 -0400

    DOM Sizes & Scrolling

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const first = document.querySelector('li');
 const second = document.getElementById('second-li');
 const button = document.querySelector('button');
-// Node List
-const lis = document.querySelectorAll('li');
+const scrollable = document.querySelector('#scrollable');
+const lis = document.querySelectorAll('li'); // Node List
 const p = document.createElement('p');
 const text = document.createTextNode('Helooooo World');
 // p.textContent = 'Helooooo Nurse';
@@ -62,21 +62,46 @@ p.append(text);
 
 
 //  GOOD - ALT, using fragment
-const parent = document.querySelector('ol');
-const fragment = document.createDocumentFragment();
-const lisToAdd = [];
-for (let i = 0; i < 3; i++) {
-    const li = document.createElement('li');
-    li.textContent = `List item with index: ${i}`;
-    fragment.append(li);
-}
-parent.append(fragment);
+// const parent = document.querySelector('ol');
+// const fragment = document.createDocumentFragment();
+// const lisToAdd = [];
+// for (let i = 0; i < 3; i++) {
+//     const li = document.createElement('li');
+//     li.textContent = `List item with index: ${i}`;
+//     fragment.append(li);
+// }
+// parent.append(fragment);
 
 // REMOVE elements
 // Verbose method
 // lis[0].parentNode.removeChild(lis[2]);
 
 // Simplified 
-lis[2].remove();
+// lis[2].remove();
 
-// DOM Sizes
+// DOM - Get font size
+console.log(window.innerWidth);
+lis[1].classList.add('big'); // 2em = 16px * 2;
+console.log(window.getComputedStyle(lis[1]).fontSize);
+
+// HEIGHT
+console.log(scrollable.clientHeight); //client =content + padding + margin
+console.log(scrollable.offsetHeight); //offset =client + border + scrollbar
+
+// 353px
+console.log(scrollable.scrollHeight); // measurement of the height of an element's content, including content not visible on the screen due to overflow.
+// 139px
+console.log(scrollable.offsetTop); // distance from the top of the page.
+console.log(scrollable.querySelectorAll('p')[0].offsetTop); // 26px
+// scrollable.querySelectorAll('p')[0].style.marginTop = 0;
+// scrollable.style.paddingTop = 0;
+// console.log(scrollable.querySelectorAll('p')[0].offsetTop); // 0px
+
+scrollable.querySelectorAll('p')[5].scrollIntoView();
+
+setTimeout(() => {
+    scrollable.scrollTo({
+        top: scrollable.querySelectorAll('p')[1].offsetTop,
+        behavior: 'smooth'
+    });
+}, 700);
